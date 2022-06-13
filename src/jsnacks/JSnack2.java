5c7e28c140213271f36e5fbfc5dfa372d9b4e5ca
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-13 11:02:07 +0200

    completed jsnack2

--- src/jsnacks/JSnack2.java
+++ src/jsnacks/JSnack2.java
@@ -1,10 +1,25 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack2 {
-
+	/*
+	 *	Chiedi all'utente di inserire un numero, se è pari stampa il numero,
+	 *	se è dispari stampa il numero successivo
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		int num = scan.nextInt();
+		
+		if (num % 2 == 0) {
+			System.out.println(num + " è pari.");
+		} else {
+			System.out.println("il numero successivo di " + num + " è " + (num + 1) + ".");
+		}
+		
+		scan.close();
 	}
 
 }
