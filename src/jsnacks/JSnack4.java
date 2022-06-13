33dc218752b3046b25df7664e8e13013b840395d
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-13 11:39:27 +0200

    completed jsnack4

--- src/jsnacks/JSnack4.java
+++ src/jsnacks/JSnack4.java
@@ -1,10 +1,24 @@
 package jsnacks;
 
 public class JSnack4 {
+	/*
+	 * 	Crea un array di numeri interi
+	 *	e fai la somma di tutti gli elementi che sono in posizione dispari
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] numbers = {4, 6, 9, 22, 42, 67, 15, 21, 64, 23, 7, 1};
+		int sum = 0;
+		
+		for (int i=0; i < numbers.length; i++) {
+			if (i % 2 == 0) {
+				sum = sum + numbers[i];
+				System.out.println("somma fino alla posizione " + (i+1) + ": " + sum);
+			}
+			
+		}
+		
 	}
 
 }
