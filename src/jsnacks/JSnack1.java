af0c4031cecf4864ad555a703f09e2ee529fe531
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-13 10:54:08 +0200

    completed jsnack1

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,10 +1,32 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack1 {
+	/*
+	 * Il software deve chiedere per 5 volte all’utente di inserire un numero.
+	 * Il programma stampa la somma di tutti i numeri inseriti.
+	 * Esegui questo programma in due versioni, con il for e con il while.
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		int sum = 0;
+		int[] numbers;
+		numbers = new int[5];
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Inserisci il " + (i+1) + "° numero: ");
+			int input = scan.nextInt();
+			numbers[i] = input;
+			sum = sum + input;
+		}
+		
+		System.out.println("Hai scelto i numeri: " + numbers[0] + "; " + numbers[1] + "; " + numbers[2] + "; " + numbers[3] + "; " + numbers[4] + "; " );
+		System.out.println("La loro somma è: " + sum);
+		
+		scan.close();
 	}
 
 }
